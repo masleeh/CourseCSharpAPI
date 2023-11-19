7b7a2a29aa5fa971514fe980ee9add7806a8c7e0
masleeh <102211370+masleeh@users.noreply.github.com>
2023-11-19 18:37:41 +0300

    Added EFCore, Hotels & Countries Tables

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,6 +1,13 @@
+using HotelListingAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDBConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options => {
+    options.UseSqlServer(connectionString);
+});
 
 // Add services to the container.
 
