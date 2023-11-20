12a6bacf46b874cc2081ba0a034d88c352638a86
masleeh <102211370+masleeh@users.noreply.github.com>
2023-11-20 16:21:13 +0300

    Added scaffoled controllers

--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -9,5 +9,32 @@ namespace HotelListingAPI.Data {
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                    new Country {
+                        Id = 1, Name = "Russia", ShortName = "RUS"
+                    },
+                    new Country {
+                        Id = 2, Name = "Georgia", ShortName = "GE"
+                    },
+                    new Country {
+                        Id = 3, Name = "Turkey", ShortName = "TUR"
+                    }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                    new Hotel {
+                        Id = 1, Name = "Pardaras Hotel", Address = "Tula", Rating = 4.5, CountryId = 1
+                    },
+                    new Hotel {
+                        Id = 2, Name = "Surf Hostekl", Address = "Batumi", Rating = 4.7, CountryId = 2
+                    },
+                    new Hotel {
+                        Id = 3, Name = "Fabrika", Address = "Tbilisi", Rating = 4.5, CountryId = 2
+                    }
+                );
+        }
     }
 }
